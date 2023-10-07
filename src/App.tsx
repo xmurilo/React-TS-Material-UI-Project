80f8fc7ee2926b077b27ed6545eb041a8441c1aa
Murilo <dixmurilox@gmail.com>
2023-10-07 13:32:37 -0300

    feat add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
+
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/initial-page" element={<p>Initial Page</p>} />
+      <Route path="*" element={<Navigate to="/initial-page" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
